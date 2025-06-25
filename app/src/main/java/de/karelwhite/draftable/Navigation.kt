72a2e3a66a7c1ff7d1f0b708bacb7ed4297e5798
DrSleez <carl.weiss@trepdeck.de>
2025-06-25 23:22:25 +0200

    add navigation for navcontroller

---
+++ app/src/main/java/de/karelwhite/draftable/Navigation.kt
@@ -0,0 +1,9 @@
+package de.karelwhite.draftable
+
+object AppDestinations {
+    const val START_SCREEN_ROUTE = "start_screen"
+    const val CREATE_TOURNAMENT_ROUTE = "create_tournament"
+    const val FIND_TOURNAMENTS_ROUTE = "find_tournaments"
+    const val SETTINGS_ROUTE = "settings"
+    const val TOURNAMENT_HISTORY_ROUTE = "tournament_history"
+}
